9114addf6cd96413faa4c4b360818d782667751b
recktard <recktarded@gmail.com>
2019-11-22 15:21:20 +0100

    new script sleeplock

--- pmenu.sh
+++ pmenu.sh
@@ -2,7 +2,7 @@
 
 MENU="$(rofi -sep "|" -dmenu -i -p 'System' -location 3 -yoffset 25 -xoffset -25 -width 15 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 -font "IBMPlex Mono 10" <<< "Sleep|Logout|Reboot|Shutdown")"
             case "$MENU" in
-                *Sleep) ~/nextcloud/Linux/scripts/i3lock-suspend;;
+                *Sleep) ~/nextcloud/Linux/scripts/sleeplock;;
                 *Logout) i3-msg exit;;
                 *Reboot) reboot;;
                 *Shutdown) shutdown now 
